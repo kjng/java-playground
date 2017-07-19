06feb3426d958ad3a094b576dfae53d1a8747d15
Kevin Jang <pianistkevinjang@gmail.com>
2017-07-19 14:00:13 -0400

    Complete nested-ifs demo app

---
+++ pluralsight/java-fundamentals/NestedIfs/src/com/kevinjang/nestedifs/Main.java
@@ -0,0 +1,19 @@
+package com.kevinjang.nestedifs;
+
+public class Main {
+
+    public static void main(String[] args) {
+        float students = 0.0f;
+        float rooms = 4.0f;
+
+        if (students > 0.0f) {
+            if (rooms > 0.0f) {
+                System.out.println(students / rooms);
+            }
+        } else {
+            System.out.println("No students");
+        }
+
+        System.out.println("\n** end program **");
+    }
+}

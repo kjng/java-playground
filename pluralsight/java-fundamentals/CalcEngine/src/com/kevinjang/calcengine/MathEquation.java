1a70a14d3975257864f5619aabd9283cd49857c0
Kevin Jang <pianistkevinjang@gmail.com>
2017-07-21 10:45:49 -0400

    Redo CalcEngine in Rational Application Developer

--- pluralsight/java-fundamentals/CalcEngine/src/com/kevinjang/calcengine/MathEquation.java
+++ pluralsight/java-fundamentals/CalcEngine/src/com/kevinjang/calcengine/MathEquation.java
@@ -2,51 +2,51 @@ package com.kevinjang.calcengine;
 
 class MathEquation {
 
-    private double leftVal;
-    private double rightVal;
-    private char opCode = 'a';
-    private double result;
+	private double leftVal;
+	private double rightVal;
+	private char opCode = 'a';
+	private double result;
 
-    double getLeftVal() { return leftVal; }
-    void setLeftVal(double leftVal) { this.leftVal = leftVal; }
-    double getRightVal() { return rightVal; }
-    void setRightVal(double rightVal) { this.rightVal = rightVal; }
-    char getOpCode() { return opCode; }
-    void setOpCode(char opCode) { this.opCode = opCode; }
-    double getResult() { return result; }
+	double getLeftVal() { return leftVal; }
+	void setLeftVal(double leftVal) { this.leftVal = leftVal; }
+	double getRightVal() { return rightVal; }
+	void setRightVal(double rightVal) { this.rightVal = rightVal; }
+	char getOpCode() { return opCode; }
+	void setOpCode(char opCode) { this.opCode = opCode; }
+	double getResult() { return result; }
 
-    MathEquation() {}
+	MathEquation() {}
 
-    private MathEquation(char opCode) {
-        this.opCode = opCode;
-    }
+	private MathEquation(char opCode) {
+		this.opCode = opCode;
+	}
 
-    MathEquation(char opCode, double leftVal, double rightVal) {
-        this(opCode);
-        this.leftVal = leftVal;
-        this.rightVal = rightVal;
-    }
+	MathEquation(char opCode, double leftVal, double rightVal) {
+		this(opCode);
+		this.leftVal = leftVal;
+		this.rightVal = rightVal;
+	}
 
-    void execute() {
+	void execute() {
 
-        switch (opCode) {
-            case 'a':
-                result = leftVal + rightVal;
-                break;
-            case 's':
-                result = leftVal - rightVal;
-                break;
-            case 'm':
-                result = leftVal * rightVal;
-                break;
-            case 'd':
-                result = rightVal != 0.0d ? leftVal / rightVal : 0.0d;
-                break;
-            default:
-                System.out.println("Unrecognized opCode");
-                result = 0.0d;
-        }
+		switch (opCode) {
+		case 'a':
+			result = leftVal + rightVal;
+			break;
+		case 's':
+			result = leftVal - rightVal;
+			break;
+		case 'm':
+			result = leftVal * rightVal;
+			break;
+		case 'd':
+			result = rightVal != 0.0d ? leftVal / rightVal : 0.0d;
+			break;
+		default:
+			System.out.println("Unrecognized opCode");
+			result = 0.0d;
+		}
 
-    }
+	}
 
 }

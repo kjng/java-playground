04a2a58ae22c00a3263ffabb396958198a89de0b
Kevin Jang <pianistkevinjang@gmail.com>
2017-07-24 09:52:59 -0400

    Implement CalculateHelper for string calculations with enum and replacement toString with String.format

--- pluralsight/java-fundamentals/CalcEngine/src/com/kevinjang/calcengine/Main.java
+++ pluralsight/java-fundamentals/CalcEngine/src/com/kevinjang/calcengine/Main.java
@@ -7,13 +7,30 @@ package com.kevinjang.calcengine;
 public class Main {
 
 	public static void main(String[] args) {
+		
+		String[] statements = {
+			"divide 100.0 50.0",
+			"add 25.0 92.0",
+			"subtract 225.0 17.0",
+			"multiply 11.0 3.0"
+		};
+		
+		CalculateHelper helper = new CalculateHelper();
+		
+		System.out.println("CalculateHelper results");
+		
+		for (String statement : statements) {
+			helper.process(statement);
+			System.out.println(helper.toString());
+		}
+		
 		MathEquation[] equations = new MathEquation[4];
 		equations[0] = new MathEquation('d', 100.0d, 50.0d);
 		equations[1] = new MathEquation('a', 25.0d, 92.0d);
 		equations[2] = new MathEquation('s', 225.0d, 17.0d);
 		equations[3] = new MathEquation('m', 11.0d, 3.0d);
 
-		System.out.println("Equation results");
+		System.out.println("\nEquation results");
 		
 		for (MathEquation equation : equations) {
 			equation.execute();
---
+++ pluralsight/java-fundamentals/CalcEngine/src/com/kevinjang/calcengine/MathCommand.java
@@ -0,0 +1,8 @@
+package com.kevinjang.calcengine;
+
+public enum MathCommand {
+	Add,
+	Subtract,
+	Multiply,
+	Divide
+}

d3fd50c0499eb18276381b433ef4fb1d51031ffd
Kevin Jang <pianistkevinjang@gmail.com>
2017-08-02 10:57:47 -0400

    Create average method accepting variable number of arguments

--- youtube/java/Apples/src/Apples.java
+++ youtube/java/Apples/src/Apples.java
@@ -1,23 +1,13 @@
-import java.util.Random;
-
-
 public class Apples {
 
 	public static void main(String[] args) {
 		
-		Random rand = new Random();
-		int freq[] = new int[7];
+		Tuna tunaObj = new Tuna();
 		
-		for (int i = 0; i < 1000; i++) {
-			++freq[rand.nextInt(6) + 1];
-		}
+		System.out.println(tunaObj.toMilitary());
+		tunaObj.setTime(11, 1, 11);
+		System.out.println(tunaObj.toMilitary());
 		
-		System.out.println("Face\tFrequency");
-		
-		for (int j = 1; j < freq.length; j++) {
-			System.out.println(j + "\t" + freq[j]);
-		}
-
 	}
 
 }

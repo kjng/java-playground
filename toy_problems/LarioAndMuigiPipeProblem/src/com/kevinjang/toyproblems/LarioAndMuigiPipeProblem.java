dc26edc4feb3d7ade8894965a6d1483a44f484da
Kevin Jang <pianistkevinjang@gmail.com>
2017-08-11 10:24:09 -0400

    Add tests

--- toy_problems/LarioAndMuigiPipeProblem/src/com/kevinjang/toyproblems/LarioAndMuigiPipeProblem.java
+++ toy_problems/LarioAndMuigiPipeProblem/src/com/kevinjang/toyproblems/LarioAndMuigiPipeProblem.java
@@ -1,17 +1,14 @@
 package com.kevinjang.toyproblems;
 
-import java.util.Arrays;
-
-/**
- * @author sfk1j
- *
- */
 public class LarioAndMuigiPipeProblem {
+	
+	// https://www.codewars.com/kata/56b29582461215098d00000f/train/java
 
 	public static void main(String[] args) {
 		
-		int[] result = pipeFix(new int[] {1, 4, 9});
-		System.out.println(Arrays.toString(result));
+		Helpers.runTest(pipeFix(new int[] {1, 4, 9}), new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9});;
+		Helpers.runTest(pipeFix(new int[] {1, 2, 3, 12}), new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12});
+		Helpers.runTest(pipeFix(new int[] {-1,4}), new int[] {-1,0,1,2,3,4});
 		
 	}
 	

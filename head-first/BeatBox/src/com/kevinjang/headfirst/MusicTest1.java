f39b36b46b9e135540d060a8c71ee5b14de06e9a
Kevin Jang <pianistkevinjang@gmail.com>
2017-08-18 14:09:54 -0400

    Create MusicTest1 catching exception

---
+++ head-first/BeatBox/src/com/kevinjang/headfirst/MusicTest1.java
@@ -0,0 +1,23 @@
+package com.kevinjang.headfirst;
+
+import javax.sound.midi.MidiSystem;
+import javax.sound.midi.MidiUnavailableException;
+import javax.sound.midi.Sequencer;
+
+public class MusicTest1 {
+	
+	public void play() {
+		try {
+			Sequencer sequencer = MidiSystem.getSequencer();
+			System.out.println("We got the sequencer!");
+		} catch (MidiUnavailableException e) {
+			System.out.println("Bummer");
+		}
+	}
+
+	public static void main(String[] args) {
+		MusicTest1 mt = new MusicTest1();
+		mt.play();
+	}
+
+}

4a910b14cd85de8a00cbe94c94e04cdcd9099fe3
Kevin Jang <pianistkevinjang@gmail.com>
2017-08-18 15:27:33 -0400

    Update with user prompt for note

--- head-first/BeatBox/src/com/kevinjang/headfirst/MiniMiniMusicApp.java
+++ head-first/BeatBox/src/com/kevinjang/headfirst/MiniMiniMusicApp.java
@@ -1,5 +1,8 @@
 package com.kevinjang.headfirst;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 import javax.sound.midi.*;
 
 public class MiniMiniMusicApp {
@@ -10,22 +13,28 @@ public class MiniMiniMusicApp {
 	}
 	
 	public void play() {
-		try {
-			Sequencer player = MidiSystem.getSequencer();
-			player.open();
-			
-			Sequence seq = new Sequence(Sequence.PPQ, 4);
-			Track track = seq.createTrack();
-			
-			ShortMessage b = new ShortMessage();
-			b.setMessage(144, 1, 44, 100);
-			MidiEvent noteOff = new MidiEvent(b, 16);
-			track.add(noteOff);
-			
-			player.setSequence(seq);
-			player.start();
-		} catch (Exception e) {
-			e.printStackTrace();
+		while (true) {
+			try {
+				Sequencer player = MidiSystem.getSequencer();
+				player.open();
+				
+				Sequence seq = new Sequence(Sequence.PPQ, 4);
+				Track track = seq.createTrack();
+				
+				BufferedReader is = new BufferedReader(new InputStreamReader(System.in));
+				System.out.print("Enter a note to play: ");
+				int note = Integer.parseInt(is.readLine());
+				
+				ShortMessage b = new ShortMessage();
+				b.setMessage(144, 1, note, 100);
+				MidiEvent noteOff = new MidiEvent(b, 1);
+				track.add(noteOff);
+				
+				player.setSequence(seq);
+				player.start();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}			
 		}
 	}
 
